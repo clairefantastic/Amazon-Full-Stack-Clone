a4de842b0897133bf1c2c06fca23e33e146982b1
Claire Chiou <clairefantastic7@gmail.com>
2024-06-24 15:53:38 +0800

    Set up firebase

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAO-7hiiFpGogoryzoxPByeRyw-vOuaaD8",
+    authDomain: "clone-6b855.firebaseapp.com",
+    projectId: "clone-6b855",
+    storageBucket: "clone-6b855.appspot.com",
+    messagingSenderId: "98276476399",
+    appId: "1:98276476399:web:7d315ff939418642bae21a",
+    measurementId: "G-BRXXGF9JF1"
+  };
