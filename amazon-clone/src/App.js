aaf35c01af1096dadab460ac2e1c7b62513187fb
Claire Chiou <clairefantastic7@gmail.com>
2024-06-25 17:15:47 +0800

    Initialize header component

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,10 +1,14 @@
+import React from "react";
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     // BEM
     <div className="app">
       <h1>Hello world</h1>
+      <Header />
+      {/* Home */}
     </div>
   );
 }
---
+++ amazon-clone/src/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <img className="header__logo" src="https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png"/>
+        <div className="header__search">
+            <input className="header__searchInput" type="text" />
+        </div>
+        <div className="header__nav">
+            <div className="header__option">
+                <span className="header__optionLineOne">Hello</span>
+                <span className="header__optionLineTwo">Sign In</span>
+            </div>
+            <div className="header__option">
+                <span className="header__optionLineOne">Returns</span>
+                <span className="header__optionLineTwo">& Orders</span>
+            </div>
+            <div className="header__option">
+                <span className="header__optionLineOne">Your</span>
+                <span className="header__optionLineTwo">Prime</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
