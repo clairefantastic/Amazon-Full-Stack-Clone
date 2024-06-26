12c951f1ef4f82fbd3f816ba2a2bf3d6626f3f36
Claire Chiou <clairefantastic7@gmail.com>
2024-06-26 14:50:31 +0800

    Add the first product

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Home.css';
+import Product from "./Product";
 
 function Home() {
   return (
     <div className="home">
         <div className="home__container">
             <img className="home__image" src="../images/amazon_home.jpg" />
+            <div className="home__row">
+                <Product />
+                {/* Product */}
+            </div>
+
+            <div className="home__row">
+                {/* Product */}
+                {/* Product */}
+                {/* Product */}
+            </div>
+
+            <div className="home__row">
+                {/* Product */}
+            </div>
         </div>
     </div>
   )
