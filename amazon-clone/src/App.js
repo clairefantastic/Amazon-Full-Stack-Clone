5f5b91728e78d5044c427acb2dce1e7eec62d731
Claire Chiou <clairefantastic7@gmail.com>
2024-06-26 16:37:41 +0800

    Add router, routes, and route

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,14 +2,29 @@ import React from "react";
 import './App.css';
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM
+    <Router>
     <div className="app">
-      <Header />
-      <Home />
+      <Routes>
+        <Route path="/checkout" element={<Header/>}> 
+          {/* Chekcout Page */}
+        </Route>
+        <Route 
+          path="/" 
+          element={
+            <>
+              <Header />
+              <Home />
+            </>
+          }> 
+        </Route>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
