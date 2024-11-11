a5bc0cf6f13618e34b944ff882c734050fa3aa4d
Claire Chiou <clairefantastic7@gmail.com>
2024-11-11 15:51:15 -0600

    Finish checkout page

--- amazon-clone/src/CheckoutProduct.js
+++ amazon-clone/src/CheckoutProduct.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import './CheckoutProduct.css'
+import { useStateValue } from './StateProvider';
 
 function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{ basket}, dispatch] = useStateValue();
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id
+    })
+  }
+
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} />
@@ -18,7 +27,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
               <p key={i}>🌟</p>
             ))}
         </div>
-        <button>Remove from Basket</button>  
+        <button onClick={removeFromBasket}>Remove from Basket</button>  
       </div>
     </div>
   )
