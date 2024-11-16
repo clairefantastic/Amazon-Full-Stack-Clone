5a78f4609ce3ddcdecb40d7b656cf78ad5969f65
Claire Chiou <clairefantastic7@gmail.com>
2024-11-16 15:36:17 -0600

    Finish login page UI and data flow

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -3,19 +3,33 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Login from "./Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Switch } from "@mui/material";
 
 function App() {
   return (
     // BEM
     <Router>
-    <div className="app">
-      <Header />
-      <Routes>
-        <Route path="/checkout" element={<Checkout />}></Route>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </div>
+      <div className="app">
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/checkout" element={
+            <>
+              <Header />
+              <Checkout />
+            </>
+            }>
+          </Route>
+          <Route path="/" element={
+            <>
+              <Header />
+              <Home />
+            </>
+            }>
+          </Route>
+        </Routes>
+      </div>
     </Router>
   );
 }
